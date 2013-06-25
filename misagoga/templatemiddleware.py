38c7f6e3b5bd2e16df6f90e1f2fb71826619c051
Ralfp <kontakt@rpiton.com>
2013-06-25 02:14:47 +0200

    Updated hook to work with Misago 0.4 new API

--- misagoga/templatemiddleware.py
+++ misagoga/templatemiddleware.py
@@ -1,8 +1,9 @@
 from django.conf import settings
+from misago.template.loader import render_to_string
 
 class GoogleAnalyticsMiddleware(object):
-    def process_context(self, theme, templates, context):
+    def process_context(self, templates, context):
         if 'hook_append_extra' in context:
-            context['hook_append_extra'] += theme.render_to_string('ga_script.html',
-                                                                   {'GoogleAnalyticsID': settings.GOOGLE_ANALYTICS_ID})
+            context['hook_append_extra'] += render_to_string('ga_script.html',
+                                                             {'GoogleAnalyticsID': settings.GOOGLE_ANALYTICS_ID})
             return context
